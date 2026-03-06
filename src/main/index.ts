40dce77ab6ad97ac7f18b5042cbd5f632e33ff99
Michael Crispen <mcrispen@crispywebservices.com>
2026-03-06 12:32:45 -0700

    updated css

--- src/main/index.ts
+++ src/main/index.ts
@@ -56,7 +56,7 @@ function createWindow(isAuthenticated: boolean) {
       contextIsolation: true,
       sandbox: false,
       preload: path.join(__dirname, "../preload/index.js"),
-      devTools: true,
+      devTools: false,
       webSecurity: true, // Always enable web security for safety
       allowRunningInsecureContent: false, // Disable insecure content
     },
